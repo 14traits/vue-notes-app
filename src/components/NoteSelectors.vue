327d78c795f813e88df8e23c744b482c4719ffe7
John Rogers <john.rogers@mac.com>
2022-08-08 13:38:12 -0500

    ++ v:binds for notes

--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -5,13 +5,21 @@ export default {
   components: {
     NoteSelector,
   },
+  data: function () {
+    return {
+      notes: [
+        { id: 1, body: "this is a test", timestamp: Date.now() },
+        { id: 2, body: "this is the second test", timestamp: Date.now() },
+        { id: 3, body: "this is the third test", timestamp: Date.now() },
+        { id: 4, body: "this is the fourth test", timestamp: Date.now() },
+      ],
+    };
+  },
 };
 </script>
 
 <template>
   <div class="note-selectors">
-    <NoteSelector />
-    <NoteSelector />
-    <NoteSelector />
+    <NoteSelector v-for="note in notes" v-bind:note="note" v-bind:key="note.id" />
   </div>
 </template>
